af16c8046482b032e569e60c08b6e9a226c1948a
Daniel Schaal <daniel@schaal.email>
2015-09-03 15:31:04 +0200

    Rename desktop file, install it

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ import xdg.BaseDirectory, os
 
 resource = "de.fotoschaal.presentor"
 
-if os.geteuid == 0:
+if os.access("/usr/share", os.W_OK | os.X_OK):
     data_path = os.path.join("/usr/share",resource)
     icon_path = os.path.join("/usr/share/icons/hicolor/scalable/apps")
+    desktop_path = os.path.join("/usr/share/applications")
 else:
     data_path = xdg.BaseDirectory.save_data_path("de.fotoschaal.presentor")
     icon_paths = xdg.BaseDirectory.load_data_paths("icons")
@@ -23,6 +24,7 @@ setup(
 
     data_files = [
         (data_path, ['data/logo.svg']),
-        (icon_path, ['data/presentor.svg'])
+        (icon_path, ['data/presentor.svg']),
+        (desktop_path, ['data/de.fotoschaal.presentor.desktop'])
     ]
 )
