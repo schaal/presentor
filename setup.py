1cc6491df9e4709906c482d584bbc546bb931d00
Daniel Schaal <daniel@schaal.email>
2017-02-17 15:20:02 +0100

    Don't install data files from setuptools

--- setup.py
+++ setup.py
@@ -1,30 +1,10 @@
 from setuptools import setup, find_packages
-import xdg.BaseDirectory, os
-
-from presentor.constants import __app_id__
-
-sys_share = '/usr/share'
-
-if os.access(sys_share, os.W_OK | os.X_OK):
-    data_path = os.path.join(sys_share,__app_id__)
-    icon_path = os.path.join(sys_share, "icons/hicolor/scalable/apps")
-    desktop_path = os.path.join(sys_share, "applications")
-else:
-    data_path = xdg.BaseDirectory.save_data_path(__app_id__)
-    icon_path = xdg.BaseDirectory.save_data_path("icons/hicolor/scalable/apps")
-    desktop_path = xdg.BaseDirectory.save_data_path("applications")
 
 setup(
-    name = "Presentor",
-    version = "0.1",
-    packages = find_packages(),
-    entry_points = {
+    name="Presentor",
+    version="0.1",
+    packages=find_packages(),
+    entry_points={
         'gui_scripts': ['presentor=presentor.presentor:main']
     },
-
-    data_files = [
-        (data_path, ['data/logo.svg']),
-        (icon_path, ['data/{0}.svg'.format(__app_id__)]),
-        (desktop_path, ['data/{0}.desktop'.format(__app_id__)])
-    ]
 )
